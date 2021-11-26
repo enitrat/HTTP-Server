a084336130a6c9696e6d5b9daada801b42f13344
msaug <msaug@protonmail.com>
2021-11-26 09:17:21 +0100

    Javadoc in repo

---
+++ doc/Javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"http.server","l":"WebServer.ClientHandler"},{"p":"http.server","l":"HandleRequest"},{"p":"http.server","l":"WebServer"}]
